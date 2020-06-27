1840c79ee60fa816846a6bfcf54e95ee04d37541
Katheeskumar Sithamparapillai <katheeskumar@outlook.com>
2020-06-27 19:26:58 +0530

    home tag line changed

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,13 +6,14 @@ const HomeController = {
 				  <html>
 					<head>
 						<title>OpShit</title>
-						<link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;500&display=swap" rel="stylesheet">
+						<link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300&display=swap" rel="stylesheet">
 					</head>
 					<body>
 						<center>
 							<h1 style="font-family: 'Rajdhani', sans-serif; font-size: 150px;">Opshit</h1>
 							<br>
-							<h1 style="font-family: 'Rajdhani', sans-serif;"">The NodeJS Framework for Powerful Back-End</h1>
+							<h1 style="font-family: 'Rajdhani', sans-serif;"">The NodeJS Framework for Smart Back-End</h1>
+							<p>It's a open source </p>
 						</center>
 					</body>
 				  </html>`;
