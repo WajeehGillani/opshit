c4a5f0d7d140e91ee0f47b1ff836b103144e6642
i2me <ikatheesh@outlook.com>
2020-07-13 12:50:36 +0530

    added vue js

---
+++ config/autoload.js
@@ -0,0 +1,27 @@
+/*
+
+clear();
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('opshit', { horizontalLayout: 'full' })
+  )
+);
+
+
+if (files.directoryExists('node_modules')) {
+  console.log(chalk.green('Already initialized node_modules!'));
+  process.exit();
+}
+else
+{
+  console.log(chalk.red('Already initialized node_modules!'));
+}*/
+
+
+/*const run = async () => {
+  const credentials = await kernel.askGithubCredentials();
+  console.log(credentials);
+};
+
+run();*/
