772ef903d67eebf3f1424675b5502f616fa7033b
Vijay <evijay77@gmail.com>
2020-07-22 09:19:57 -0700

    login auth

--- public/index.js
+++ public/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose')
 const routes = require('../routes');
 const env = require('dotenv').config({ path: '../.env' });
 
 const app = express();
 let public = __dirname;
-//console.log(public)
+
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true}, () =>{
+    console.log('Connected to MongoDB...');
+});
+
+app.u
 app.use(cors());
 app.use(express.static(public));
 app.use('/', routes);
