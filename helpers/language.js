e827a37a80c05b437fc5513f7feecffd82df1a0e
dev-PC\dev <nadeera3784@gmail.com>
2020-07-29 07:14:28 -0700

    feat : language files added

---
+++ helpers/language.js
@@ -0,0 +1,14 @@
+const path    = require('path');
+
+const languageHelper = async function () {
+    let i18n = new (require('i18n-2'))({
+        locales: ['en', 'de', 'es', 'ru'],
+        directory: path.join(__dirname, '../language/'),
+        devMode : false
+    });
+    return i18n;
+}
+
+module.exports = {
+    languageHelper,
+};
