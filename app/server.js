0ec462d2cb51bd851ad4bc710f5719234e028aa8
dev-PC\dev <nadeera3784@gmail.com>
2020-07-29 07:20:22 -0700

    feat : helper function added

--- app/server.js
+++ app/server.js
@@ -8,18 +8,56 @@ const moment  = require('moment');
 
 const routes = require('../routes');
 const env = require('dotenv').config({ path: './.env' });
+const {languageHelper}      = require('../helpers/language.js');
 
 let app = express();
+
 let public = __dirname.substring(0, __dirname.length-3) + 'public';
 
+let app_root = '/';
+
 app.use(cors());
+
 app.use(helmet());
+
 app.use(flash());
+
 app.use(express.static(public));
+
+app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.set("view engine", "ejs");
+
 app.use('/', routes);
 
+let helper = {
+	language : function (value) {
+		return languageHelper().__(value);
+	},
+	app_root : function(){
+		return app_root;
+	},
+	initial_capitalizer : function(string){
+	  return string[0].toUpperCase() +  string.slice(1); 
+	},
+	date_format : function(date, format='MM/DD/YYYY'){
+	  return moment(date).format(format);
+	},
+	truncate : function(str, len){
+		if (str.length > len){
+           return str.substring(0,5) + '...';
+		}else{
+	      return str;
+		}
+	}
+
+};
+app.use(function (request, response, next) {;
+    request.helper  = helper;
+	next();
+});
 app.disable('x-powered-by');
 
 app.listen(process.env.PORT || 5001, (err) => {
